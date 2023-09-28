3f84992d3209ba1072ec67d60e328422f407f680
zorazky <oswaldo.vazquez@gmail.com>
2023-09-28 10:41:25 -0600

    css, props, events

---
+++ primer-proyecto-react/src/ContadorApp.jsx
@@ -0,0 +1,14 @@
+export const ContadorApp = () => {
+  return (
+    <>
+      <h1>Contador:</h1>
+      <button
+        onClick={function () {
+          console.log("Soy un Boton");
+        }}
+      >
+        Soy un boton
+      </button>
+    </>
+  );
+};
--- primer-proyecto-react/src/main.jsx
+++ primer-proyecto-react/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PrimerComponente } from "./PrimerComponente";
+import { ContadorApp } from "./ContadorApp";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PrimerComponente />
+    <ContadorApp />
   </React.StrictMode>
 );
